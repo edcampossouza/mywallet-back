4af25d52f6fb9267ccc2b8b4fdcdfc0002fd67d3
Eduardo Souza <edcampossouza@gmail.com>
2023-01-20 14:44:29 -0300

    userSchema: add 'email' field

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -2,11 +2,12 @@ import Joi from "joi";
 
 export const userInputSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().email({ tlds: { allow: false } }),
+  email: Joi.string().email({ tlds: { allow: true } }),
   password: Joi.string().min(3).required(),
 });
 
 export const userSchema = Joi.object({
   name: Joi.string().required(),
+  email: Joi.string().email({ tlds: { allow: true } }),
   passwordHash: Joi.string().required(),
 });
