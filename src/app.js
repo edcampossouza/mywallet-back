85b38097aecbfefedc44e995bb754966c822c135
Eduardo Souza <edcampossouza@gmail.com>
2023-01-22 19:21:23 -0300

    use process.env to get port number

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import authRouter from "./routes/authRouter.js";
 import registryRouter from "./routes/registryRouter.js";
 
 dotenv.config();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
